45070732065f52ded2e58d42208656eb1c146066
Ji Lee <jihyunlee@wustl.edu>
2017-09-11 00:53:16 -0500

    5,6

---
+++ mouse-events/round-1/five.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){
+
+$( "#div1" ).hover(function() {
+    $( "#image1" ).fadeIn( 500 );
+});
+
+$( "#div2" ).hover(function() {
+    $( "#image2" ).fadeIn( 500 );
+});
+
+$( "#div3" ).hover(function() {
+    $( "#image3" ).fadeIn( 500 );
+});
+
+$( "#div4" ).hover(function() {
+    $( "#image4" ).fadeIn( 500 );
+});
+
+$( "#div5" ).hover(function() {
+    $( "#image5" ).fadeIn( 500 );
+});
+
+$( "#div6" ).hover(function() {
+    $( "#image6" ).fadeIn( 500 );
+});
+
+$( "#div7" ).hover(function() {
+    $( "#image7" ).fadeIn( 500 );
+});
+
+$( "#div8" ).hover(function() {
+    $( "#image8" ).fadeIn( 500 );
+});
+
+$( "#div9" ).hover(function() {
+    $( "#image9" ).fadeIn( 500 );
+});
+
+$( "#div10" ).hover(function() {
+    $( "#image10" ).fadeIn( 500 );
+});
+
+
+});
---
+++ mouse-events/round-1/six.js
@@ -0,0 +1,85 @@
+$(document).ready(function(){
+    
+    $("#div1").mouseover(function(){
+        $("#image1").show();
+    });
+    
+    $("#div1").mouseout(function(){
+        $("#image1").hide();
+    });
+    
+    $("#div2").mouseover(function(){
+        $("#image2").show();
+    });
+    
+    $("#div2").mouseout(function(){
+        $("#image2").hide();
+    });
+    
+    $("#div3").mouseover(function(){
+        $("#image3").show();
+    });
+    
+    $("#div3").mouseout(function(){
+        $("#image3").hide();
+    });
+    
+    $("#div4").mouseover(function(){
+        $("#image4").show();
+    });
+    
+    $("#div4").mouseout(function(){
+        $("#image4").hide();
+    });
+
+    $("#div5").mouseover(function(){
+        $("#image5").show();
+    });
+    
+    $("#div5").mouseout(function(){
+        $("#image5").hide();
+    });
+    
+    $("#div6").mouseover(function(){
+        $("#image6").show();
+    });
+    
+    $("#div6").mouseout(function(){
+        $("#image6").hide();
+    });
+    
+    $("#div7").mouseover(function(){
+        $("#image7").show();
+    });
+    
+    $("#div7").mouseout(function(){
+        $("#image7").hide();
+    });
+    
+    $("#div8").mouseover(function(){
+        $("#image8").show();
+    });
+    
+    $("#div8").mouseout(function(){
+        $("#image8").hide();
+    });
+    
+    $("#div9").mouseover(function(){
+        $("#image9").show();
+    });
+    
+    $("#div9").mouseout(function(){
+        $("#image9").hide();
+    });
+    
+    $("#div10").mouseover(function(){
+        $("#image10").show();
+    });
+    
+    $("#div10").mouseout(function(){
+        $("#image10").hide();
+    });
+
+
+
+});
